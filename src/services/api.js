196f1a1f4695f7cdae3555a093780282912fb478
Prap1 <praphoolk32@gmail.com>
2026-04-16 15:32:23 +0530

    'updated'

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://frozen-delights-backend.vercel.app/api',
     withCredentials: true, // 🔥 cookie support
 });
 
